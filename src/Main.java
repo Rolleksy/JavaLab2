c7f9c4e0e0866d0ff318859e877cdd5200ab5d5e
Rolleksy <aleksanderpurcha@gmail.com>
2021-10-30 08:34:47 +0200

    drugi labo

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,12 @@ public class Main {
           to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
         */
 
+        do
+            {
+
+            }
+        while();
+
 
         /* zad 2
         a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
