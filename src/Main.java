09b521d275acc0384ce23a65ecc1eb9fd20695e7
Rolleksy <aleksanderpurcha@gmail.com>
2021-10-30 09:48:01 +0200

    drugi labo

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,19 @@ public class Main {
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
-
+        String tekst1 = "tekst 1";
+        String tekst2 = "tekst 2";
+        String tekst3 = "tekst 3";
+        for (int i = 0; i<10; i++){
+            switch(i){
+                case 0:
+                    System.out.println(tekst1);
+                    continue;
+                case 1:
+                    System.out.println(tekst2);
+                    break;
+            }
+        }
 
     }
 }
